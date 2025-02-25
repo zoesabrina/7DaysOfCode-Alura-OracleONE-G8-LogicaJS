2d2433c5bb782404632fc199af24de4be07df81c
Sabrina Zoe <96883466+zoesabrina@users.noreply.github.com>
2025-02-25 12:04:02 -0300

    Add files via upload
    Dia 1 - Operações Booleanas
    Trabalhei com comparações de valores e tipos de dados, usando os operadores de igualdade (== e ===), e explorando suas diferenças.

---
+++ dia1_operacoes_booleanas.js
@@ -0,0 +1,27 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+//COMPARAR O numeroUm e a stringUm
+if (numeroUm == stringUm) {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+//COMPARAR O numeroTrinta e a stringTrinta
+if (numeroTrinta === stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+//COMPARAR O numeroDez e a stringDez
+if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
